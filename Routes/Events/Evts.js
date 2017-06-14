dd196a25efd76db14e33f6d6a8f212533fb626ab
Sonia Mannan <soniamannan@pcp130017pcs.wireless.calpoly.edu>
2017-06-13 18:04:52 -0700

    sonia

--- Routes/Events/Evts.js
+++ Routes/Events/Evts.js
@@ -307,7 +307,7 @@ router.get('/:id/Rsvs', function(req, res) {
    function(evts, fields, cb) {
       console.log(evts)
 
-      if ('NULL' in evts && !(evts[0]['NULL'])) {
+      if ('NULL' in evts[0] && !(evts[0]['NULL'])) {
          console.log('HERE')
          res.json([]).end();
       }
