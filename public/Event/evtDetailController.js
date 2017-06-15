f810f7841496f3057f1cff9691758478f1a1e8b0
vstumpf <vincents.995@gmail.com>
2017-06-15 15:51:30 -0700

    event page can update reservation

--- public/Event/evtDetailController.js
+++ public/Event/evtDetailController.js
@@ -1,12 +1,15 @@
 app.controller("evtDetailController", 
- ['$scope', '$stateParams', '$state', '$http', 'notifyDlg',
- function($scope, $stateParams, $state, $http, nDlg) {
+ ['$rootScope', '$scope', '$stateParams', '$state', '$http', 'notifyDlg',
+ function($rs, $scope, $stateParams, $state, $http, nDlg) {
    var evtId = $stateParams.evtId;
-  
+   var pid = $rs.user.id;
+   var myrsv = null;
+
    // Get event information
    $http.get('/Evts/' + evtId)
    .then(function(response) {
       $scope.events = response.data;
+      console.log($scope.events);
    })
    .catch(function(err) {
       $scope.events = null;
@@ -15,31 +18,49 @@ app.controller("evtDetailController",
    // Get all reservations for this event 
    $http.get('/Evts/' + evtId + '/Rsvs')
    .then(function(response) {
-      $scope.reservations = response.data;
+      $scope.rsvs = response.data;
+      console.log($scope.rsvs);
    })
    .catch(function(err) {
       $scope.rsvs = null;
    });
-   
-   $scope.createEvent = function() {
-      if (!$scope.newEvent) {
-         nDlg.show($scope, "Error: No Reservation for Event " +
-          evtId, "Error");
-      }
-      else {
-         $http.post('/Evts/' + evtId + '/Msgs', {content: $scope.newEvent})
-         .then(function() {
-            $scope.newEvent = null;
-            //return $http.get('/Cnvs/' + evtId + '/Msgs')
-         })
-         .then(function(response) {
-            $scope.messages = response.data;
-         })
-         .catch(function(err) {
-            if (err.data[0].tag) {
-               nDlg.show($scope, "Error: " + err.data[0].tag, "Error");
-            }
-         })
-      }
+
+   var getMyRsv = function () {
+      return $http.get('/Prss/' + pid + '/Rsvs')
+      .then(function(response) {
+         response.data.forEach(function(rsvtemp) {
+            if (rsvtemp.evtId === parseInt(evtId))
+               myrsv = rsvtemp;
+         });
+      });
+   };
+   getMyRsv();
+
+   $scope.checkRsv = function(s) {
+      return myrsv ? myrsv.status === s : false;
+   };
+
+   $scope.changeMyRsv = function(s) {
+      (function() {
+         if (!myrsv) {
+            return $http.post('/Evts/' + evtId + '/Rsvs', 
+             {prsId: pid, evtId: evtId});
+         } else {
+            var rid = myrsv.id;
+            return $http.put('/Prss/' + pid + '/Rsvs/' + rid, {status: s});
+         }
+      })()
+      .then(function() {
+         return $http.get('Evts/' + evtId + '/Rsvs');
+      })
+      .then(function(response) {
+         $scope.rsvs = response.data;
+         return getMyRsv();
+      })
+      .catch(function(err) {
+         if (err.data[0].tag) {
+            nDlg.show($scope, "Error: " + err.data[0].tag, "Error");
+         }
+      })
    };
 }]);
